0184d60beeefc33fe00e4903eac9fd4f10a0cbf9
sambit2 <sambit_praharaj@yahoo.com>
2017-01-16 16:04:27 +0100

    Add part of werkclg 3

---
+++ WerkCollege3-17Jan2017/js/display-asq-solutions.js
@@ -0,0 +1,50 @@
+var genericFn = function(el){
+  var sol = el.querySelector('asq-solution')
+  if(! sol) return;
+
+  try{
+    el.value = sol.innerHTML
+  }catch(err){
+    console.log("Error displaying solution for" + el + '\n' + err.stack);
+  }
+}
+
+var multiChoiceFn = function(el){
+  var correctOpts = el.querySelectorAll('asq-option[correct]')
+  correctOpts = [].slice.call(correctOpts, 0)
+  correctOpts.forEach(function(opt){
+    opt.checked = true;
+  })
+}
+
+var elements = [
+  'asq-sqlite-q', 
+  {'asq-multi-choice-q':multiChoiceFn }
+]
+
+function displayAsqSolutions(){
+  for (var i = 0, l = elements.length; i < l; i++){
+    try{
+      var fn, elType;
+
+      if(typeof elements[i] == 'string'){
+        elType = elements[i];
+        fn = genericFn;
+      }else if(typeof elements[i] == 'object'){
+        //check if there is custom callback
+
+        var props =  Object.getOwnPropertyNames(elements[i]);
+        if(! props.length) continue;
+        var elType = props[0];
+        fn = elements[i][elType]
+        if(typeof fn != 'function') contine;
+      }
+      
+      var els = document.querySelectorAll(elType);
+      [].slice.call(els, 0).forEach(fn);
+
+    }catch(err){
+      console.log("Error displaying solution for" + elements[i] + ' elements \n' + err.stack);
+    }
+  }
+}
